0bf21984ff29a1ae20f1d0c0610e8d8fe01284f1
Jefferson <jefferson@educationjourney.com>
2024-09-14 14:36:58 -0300

    feat: user controller, dto, module & service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserResponse, UserDto } from './users.dto';
+
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() user: UserDto): Promise<CreateUserResponse> {
+    return await this.usersService.create(user);
+  }
+}
